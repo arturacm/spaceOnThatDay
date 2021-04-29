11042222f7d639cb6b79f439c2fb649d198a09c9
Artur Camara Moreira <arturacm@gmail.com>
2021-04-29 16:04:36 -0300

    nasa API & date selection implemented

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("teste")
+let url = "https://api.nasa.gov/planetary/apod?api_key=jXsOlWB7fEPgHYhuVbn6g7bIPXJQnaI5e2Jx1avI";
+function start(){
+    let date = $("#dateinput").val()
+    console.log(date)
+    $.ajax({url: url+"&date="+ date,
+    success: (result)=>{
+        mostrar(result)
+    }})
+}
+function mostrar(obj){
+    console.log("cheguei aqui")
+    console.log(JSON.stringify(obj))
+    $("#image").attr("src", obj.url)
+    $("h1").html(obj.title)
+}
